f126c5bf45d0605aa9b51e5eec3b658499aebb14
yab <6627294+yablochko8@users.noreply.github.com>
2024-07-04 11:45:25 -0400

    npm install --save-dev @types/lodash plus more comments

--- server/services/reviewer.ts
+++ server/services/reviewer.ts
@@ -2,23 +2,23 @@ import type {
   GithubStatus,
   GithubStatusList,
   ReviewStatus,
-} from "../types/shared"
-import { getRecentCommits } from "./callGithub"
-import { CommitSummary, CommitsByUser } from "../types/CommitSummary"
-import _ from "lodash"
-import { evaluateCommits } from "./evaluateCommits"
+} from "../types/shared";
+import { getRecentCommits } from "./callGithub";
+import { CommitSummary, CommitsByUser } from "../types/CommitSummary";
+import _ from "lodash";
+import { evaluateCommits } from "./evaluateCommits";
 
 export function summarizeStatus(statusList: GithubStatusList) {
   const userResults = statusList.reduce((acc, status) => {
     if (acc[status.user]) {
-      acc[status.user].push(status.pass)
+      acc[status.user].push(status.pass);
     } else {
-      acc[status.user] = [status.pass]
+      acc[status.user] = [status.pass];
     }
-    return acc
-  })
+    return acc;
+  });
 
-  console.log("userResults:", userResults)
+  console.log("userResults:", userResults);
 
   // const userSummary = Object.keys(userNames).map((user) => {
   //   const passes = userNames[user].filter((pass) => pass).length
@@ -27,7 +27,7 @@ export function summarizeStatus(statusList: GithubStatusList) {
 
   // console.log("userSummary:", userSummary)
 
-  return userResults
+  return userResults;
 }
 
 /**
@@ -40,44 +40,44 @@ export function transformStatus(status: GithubStatus): string {
   user: ${status.user}
   status: ${status.pass}
   pass: ${status.pass}
-  `
-  return output
+  `;
+  return output;
 }
 
 export function reviewUserCommits(CommitsByUser: CommitsByUser) {
-  const commitCount = CommitsByUser.commits.length
-  return `User: ${CommitsByUser.user} has ${commitCount} commits.`
+  const commitCount = CommitsByUser.commits.length;
+  return `User: ${CommitsByUser.user} has ${commitCount} commits.`;
 }
 
 export async function reviewCommits(commits: CommitsByUser[]): Promise<string> {
-  const reviews: string[] = []
+  const reviews: string[] = [];
 
   for (const cl of commits) {
-    const userReview = await evaluateCommits(cl)
-    reviews.push(userReview)
+    const userReview = await evaluateCommits(cl);
+    reviews.push(userReview);
   }
 
-  console.log("result:", reviews)
-  const results = reviews.join("\n\n")
-  return results
+  console.log("result:", reviews);
+  const results = reviews.join("\n\n");
+  return results;
 }
 
 function getUniqueUsers(commits: CommitSummary[]) {
-  const users = commits.map((commit) => commit.user)
-  const uniqueUsers = _.uniq(users)
-  return uniqueUsers
+  const users = commits.map((commit) => commit.user);
+  const uniqueUsers = _.uniq(users);
+  return uniqueUsers;
 }
 
 function getCommitsByUser(commits: CommitSummary[]): CommitsByUser[] {
-  const users = getUniqueUsers(commits)
+  const users = getUniqueUsers(commits);
 
-  const commitsByUser: CommitsByUser[] = []
+  const commitsByUser: CommitsByUser[] = [];
   for (const user of users) {
-    const cm = commits.filter((commit) => commit.user === user)
-    commitsByUser.push({ user, commits: cm })
+    const cm = commits.filter((commit) => commit.user === user);
+    commitsByUser.push({ user, commits: cm });
   }
 
-  return commitsByUser
+  return commitsByUser;
 }
 
 export async function getReviewStatus(): Promise<ReviewStatus> {
@@ -85,19 +85,19 @@ export async function getReviewStatus(): Promise<ReviewStatus> {
   //   text: "review text",
   //   cooking: ["one", "two", "three"],
   // }
-  const commits = await getRecentCommits()
-  const users = getUniqueUsers(commits)
+  const commits = await getRecentCommits();
+  const users = getUniqueUsers(commits);
 
-  const commitsByUser = getCommitsByUser(commits)
-  const reviews: string = await reviewCommits(commitsByUser)
+  const commitsByUser = getCommitsByUser(commits);
+  const reviews: string = await reviewCommits(commitsByUser);
 
   const status = {
     // commits,
     reviews,
     users,
     // commitsByUser,
-  }
-  console.log("getReviewStatus:", status)
+  };
+  console.log("getReviewStatus:", status);
 
-  return status
+  return status;
 }
