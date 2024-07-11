9577e144659b9e2c1e15fe482efb80e7cada1d13
Lui <6627294+yablochko8@users.noreply.github.com>
2024-07-11 17:18:21 -0400

    more names

--- server/services/github/getName.ts
+++ server/services/github/getName.ts
@@ -1,16 +1,24 @@
 export const getName = (username: string): string => {
   switch (username) {
+    case "ajroberts0417ajroberts0417":
+      return "<@U0764BM894M>";
     case "briansmiley":
       return "<@U075SN59RMM>";
     case "brunoLloret":
       return "<@U07675V6CUB>";
-    case "elizasvielelizasviel":
+    case "dcsan":
+      return "<@U07B15N075W>";
+    case "elizasviel":
       return "<@U0769P5AD5J>"; // Norman
     case "dxrendxren":
+    case "dxren":
       return "<@U075SN5BV47>";
+    case "Hendersonajardimj":
+      return "<@U0769P5LUMS>";
     case "sarahbicknell":
-      return "@Sarah";
+      return "<@U0760KZUWK0>";
     case "yablochko8":
+    case "yab":
       return "<@U076W321UJC>";
     case "yayakixtenet":
     case "yayakix":
